28f5c41a3c179ec53804bc4c32ebd13b785b1839
Samia Ahmed <23-101162@students.eui.edu.eg>
2026-06-13 12:40:06 +0300

    adding template

--- src/Components/Layout/Storytemplate.jsx
+++ src/Components/Layout/Storytemplate.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Menu_screen.css';
 import Sound_button  from '../Components/Comman/Sound_button';
 import Setting_screen from '../Components/Comman/Setting_screen';
 import Menu_button    from '../Components/Comman/Menu_button';
 
-const Menu_screen = () => {
-  const [showSettings, setShowSettings] = useState(false);
+const Menu_screen = ({ background, nextPath }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (nextPath) navigate(nextPath);
+  };
 
   return (
-    <main>
+    <main
+      className="main_intro1"
+      onClick={handleClick}
+      style={{
+        backgroundImage: `url(${background})`,
+        cursor: nextPath ? 'pointer' : 'default',
+      }}
+    >
       <Sound_button />
       <Menu_button />
-  
+
     </main>
   );
 };
